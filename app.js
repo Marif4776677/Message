27545655b720ec254202d5a85433235a5278da09
Marif Ismayilov <marifismayilovv2+demo@gmail.com>
2024-04-06 10:21:16 +0000

    done

--- app.js
+++ app.js
@@ -1,36 +1,57 @@
 "use strict";
 $(document).ready(function () {
 
-    $(document).on('keypress', function (e) {
-        if (e.which == 13) {
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myweather").val()}&appid=4196983d205ec806b2ac15e4ef5753e5&units=metric`;
-            async function myweather() {
-                let responce = await fetch(url);
-                let data = await responce.json();
-                $("#city").html(data.name);
-                $("#temp").html(data.main.temp);
-                $("#feels").html(data.main.feels_like);
-                $("#myweather").val("");
-                console.log(data)
-            }
-            myweather()
-        }
-    });
-
-    $(".btn").click(function (e) {
+    $("#me").click(function (e) {
         e.preventDefault();
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myweather").val()}&appid=4196983d205ec806b2ac15e4ef5753e5&units=metric`;
-        async function myweather() {
-            let responce = await fetch(url);
-            let data = await responce.json();
-            $("#city").html(data.name);
-            $("#temp").html(data.main.temp);
-            $("#feels").html(data.main.feels_like);
-            $("#myweather").val("");
-            console.log(data)
+        function myel(myclass = "") {
+            if (!$.trim($("#inputme").val()) == "") {
+                let vall = $("#inputme").val()
+                    , newH3 = $('<h3> </h3>')
+                    , newDiv = $('<div class = "line"> </div')
+                    , bigDiv = $(`<div ${myclass}> </div`)
+                $(newH3).append(vall)
+                $(newDiv).append(newH3)
+                $(bigDiv).append(newDiv)
+                return bigDiv
+
+            }
+            else {
+                return
+            }
+
         }
-        myweather()
+        $("#messageyou").append(myel())
+        $("#messageme").append(myel('class ="end"'))
+        $("#inputme").val("")
+
+        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight)
+        document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
 
     });
 
+    $("#you").click(function (e) {
+        e.preventDefault();
+        function myel(myclass = "") {
+            if (!$.trim($("#inputyou").val()) == "") {
+                let vall = $("#inputyou").val()
+                    , newH3 = $('<h3> </h3>')
+                    , newDiv = $('<div class = "line"> </div')
+                    , bigDiv = $(`<div ${myclass}> </div`)
+                $(newH3).append(vall)
+                $(newDiv).append(newH3)
+                $(bigDiv).append(newDiv)
+                return bigDiv
+            }
+            else {
+                return
+            }
+        }
+        $("#messageme").append(myel())
+        $("#messageyou").append(myel('class ="end"'))
+        $("#inputyou").val("")
+
+        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight)
+        document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
+
+    });
 });
